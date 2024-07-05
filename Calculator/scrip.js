5c9f8e4e1070a00058992a47b81e6fec639616a2
Le Trong Quynh <quynhle24082001@gmail.com>
2024-07-05 16:59:41 +0700

    2

---
+++ Calculator/scrip.js
@@ -0,0 +1,9 @@
+var Click = document.querySelector('#calculate')
+Click.addEventListener("click",function  MulchCalculator (){
+    var a = parseFloat(document.getElementById('length').value);
+    var b = parseFloat(document.getElementById('depth').value);
+    var Area = a * a;
+    var Volume = Area * b
+    
+});
+
